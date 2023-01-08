b097c4a08fa3e608037f2cc7d80e71ae0d265e5e
hieunm <nguyenmanhhieutl@gmail.com>
2023-01-08 23:22:59 +0700

    cap nhat

--- 103-Destructuring Arrays/main.js
+++ 103-Destructuring Arrays/main.js
@@ -4,6 +4,10 @@ const restaurant = {
     categories : ['Italian','Pizzeria','Vegetarian','Organic'],
     startermenu : ['Focaccia','Bruscheta','Garlic','Bread','Capreses Salad'],
     mainMenu : ['Pizza','Pasta','Risotto'],
+
+    order: function(starterIndex, mainIndex){
+        return [this.startermenu[starterIndex],this.mainMenu[mainIndex]]
+    },
 };
 
 const arr =  [2,3,4];
@@ -14,3 +18,13 @@ const c = arr[2];
 const [x ,y ,z] = arr;
 console.log(x,y,z);
 console.log(arr);
+let [main, secondary] = restaurant.categories;
+console.log(main,secondary);
+[main ,secondary] = [secondary, main];4
+console.log(main, secondary)
+const [starter, mainCourse] = restaurant.order(2,0);
+console.log(starter, mainCourse);
+const nested = [2, 4, [5, 6]];
+const [i, ,[j,k]] = nested;
+console.log(i,j,k);
+
