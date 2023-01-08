9be01f62899d90082918d40d17820ddb29438191
hieunm <nguyenmanhhieutl@gmail.com>
2023-01-08 23:25:28 +0700

    commit 103

--- 103-Destructuring Arrays/main.js
+++ 103-Destructuring Arrays/main.js
@@ -27,4 +27,6 @@ console.log(starter, mainCourse);
 const nested = [2, 4, [5, 6]];
 const [i, ,[j,k]] = nested;
 console.log(i,j,k);
-
+//deafault values
+const [p=1, q=1, r=1] = [8];
+console.log(p,q,r);
