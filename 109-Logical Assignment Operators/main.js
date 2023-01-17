c579701033b35419ce677071fa9b5501efc0cbcb
hieunm <nguyenmanhhieutl@gmail.com>
2023-01-17 09:32:48 +0700

    uppdate

--- 109-Logical Assignment Operators/main.js
+++ 109-Logical Assignment Operators/main.js
@@ -18,4 +18,15 @@ const restaurant = {
             close: 24,
         },
     },
+    order: function (starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
+
+    orderDelivery: function ({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+        console.log(
+            `Order received! ${this.starterMenu
+            [starterIndex]} and ${this.mainMenu[mainIndex]}
+            will be delivered to ${address} at ${time}`
+        );
+    },
 }
