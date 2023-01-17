4513e06bf0a6231e2310a2f9f2b31ea93cfca1c9
hieunm <nguyenmanhhieutl@gmail.com>
2023-01-17 10:07:05 +0700

    ok

--- 110-Coding Challenge (1)/main.js
+++ 110-Coding Challenge (1)/main.js
@@ -39,3 +39,17 @@ const game = {
             team2: 6.5,
         },
 };
+//1
+const [players1, players2] = game.players;
+console.log(players1, players2);
+
+//2
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+
+//3
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+//4
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Periscic'];
