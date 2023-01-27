d6e1eacbea6009d2acde83b8b6daf569e264aa47
hieunm <nguyenmanhhieutl@gmail.com>
2023-01-27 21:10:53 +0700

    update114

---
+++ 114-Looping Objects/main.js
@@ -0,0 +1,15 @@
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const openingHours = {
+    [weekdays[3]]: {
+        open: 12,
+        close: 22,
+    },
+    [weekdays[4]]: {
+        open: 11,
+        close: 23,
+    },
+    [weekdays[5]]: {
+        open: 0, //Open 24 hours
+        close: 24,
+    },
+};
