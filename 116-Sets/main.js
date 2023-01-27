29ea3b54dd22cc0df0cf6f659d7d583667e5241d
hieunm <nguyenmanhhieutl@gmail.com>
2023-01-27 22:09:06 +0700

    update116

--- 116-Sets/main.js
+++ 116-Sets/main.js
@@ -47,3 +47,81 @@ const restaurant = {
         console.log(otherIngredients);
     }
 };
+const ordersSet = new Set([
+    'Pasta',
+    'Pizza',
+    'Pizza',
+    'Risotto',
+    'Pasta',
+    'Pizza',
+]);
+console.log(ordersSet);
+
+console.log(new Set('jonas'));
+
+console.log(ordersSet.size);
+console.log(ordersSet.has('Pizza'));
+console.log(ordersSet.has('Bread'));
+ordersSet.add('Garlic Bread');
+ordersSet.add('Garlic Bread');
+ordersSet.delete('Risotto');
+// ordersSet.clear();
+console.log(ordersSet);
+
+for (const order of ordersSet) console.log(order);
+
+// Example
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'chef', 'Waiter'];
+const staffUnique = [...new Set(staff)];
+console.log(staffUnique);
+
+console.log(
+    new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'chef', 'Waiter']).size
+);
+
+console.log(new Set('jonasschmedtman').size);
+
+
+
+
+const game = {
+    team1: 'Bayern Munich',
+    team2: "Borrussia Dortmund",
+    players:
+        [
+            [
+                'Neuer',
+                'Pavard',
+                'Martinez',
+                'Alaba',
+                'Davies',
+                'Kimmich',
+                'Goretzka',
+                'Coman',
+                'Muller',
+                'Gnarby',
+                'Lewandowski',
+            ],
+            [
+                'Burki',
+                'Schulz',
+                'Hummels',
+                'Alanji',
+                'Hakimi',
+                'Weigl',
+                'Witsel',
+                'Hazard',
+                'Brandt',
+                'Sancho',
+                'Gotze',
+            ],
+        ],
+        score: '4:0',
+        scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+        date: 'Nov 9th, 2037',
+        odds: {
+            team1: 1.33,
+            x: 3.25,
+            team2: 6.5,
+        },
+};
