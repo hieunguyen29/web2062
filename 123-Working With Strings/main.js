a82c4ace13173f76f2858a3d60aff878be8ee565
hieunm <nguyenmanhhieutl@gmail.com>
2023-01-29 01:10:27 +0700

    commit123

--- 123-Working With Strings/main.js
+++ 123-Working With Strings/main.js
@@ -17,3 +17,27 @@ console.log(namesUpper.join(' '));
 captializeName('jessica ann smith davis')
 captializeName('jonas scheddtman')
 
+//padding
+const message = "Go to gate 23!";
+console.log(message.padStart(20,"+").padEnd(30,'+'));
+console.log('Jonas'.padStart(20,'+').padEnd(30,'+'));
+
+const maskCreditCard = function(number){
+   const str = number + '';
+   const last= str.slice(-4);
+   return last.padStart(str.length,'*');
+}
+console.log( maskCreditCard(45646546546545));
+console.log( maskCreditCard(12333211421323654654));
+console.log(maskCreditCard('12333211421323654654'));
+
+//Repeat
+const message2='Bad weather... All Depatrues Delayed....';
+console.log(message2.repeat(5));
+
+const plamesInline = function (n){
+   console.log(`There are ${n} planes in line ${'✈️'.repeat(n)}`);
+};
+plamesInline(5)
+plamesInline(3)
+plamesInline(12)
