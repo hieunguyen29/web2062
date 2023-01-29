b7d8c1b80631c4fcc96ac19d76d6a583777e48ca
hieunm <nguyenmanhhieutl@gmail.com>
2023-01-29 22:36:27 +0700

    update

--- 115-Coding Challenge #2/main.js
+++ 115-Coding Challenge #2/main.js
@@ -1,3 +1,27 @@
+// LAB1.2: (SECTION	9: DATA	STRUCTURES. MODERN	OPERATORS	AND	STRINGS	> CODING	
+//     CHANLLENGE	#2)
+//     1. Lặp mảng game.scored array và in ra cầu thủ cùng với số bàn thắng đã ghi (Ví
+//     dụ: "Goal 1: Lewandowski")
+//     2. Sử dụng vòng lặp để tính tỉ lệ kết quả trận đấu và in ra console (Chúng ta đã
+//     học cách tính trung bình tổng ở phần trước, các bạn có thể xem lại nếu không
+//     nhớ)
+//     3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+//      Tỉ lệ thắng của Bayern Munich: 1.33
+//      Tỉ lệ hòa of draw: 3.25
+//      Tỉ lệ thắng của Borrussia Dortmund: 6.5
+//     Lấy ra tên đội bóng trực tiếp từ object 'game', bạn không nên "hardcode" tỉ lệ
+//     thắng của mỗi đội (ngoại trừ trường hợp 2 đội hòa nhau).
+//     Chú ý: Lưu ý cách tỷ lệ cược và đối tượng trò chơi có tên thuộc tính giống nhau �
+//     �
+//     Thêm: Tạo ra 1 đối tướng là 'scorers', đối tượng này gồm các thuộc tính với key
+//     (tên thuộc tính) là tên cầu thủ đã ghi bàn, value (giá trị) là số bàn thắng đã ghi.
+//     Ví dụ:
+//      {
+//     Gnarby:1,
+//     Hummels:1,
+//     Lewandowski: 2
+//      }
+
 const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 const openingHours = {
     [weekdays[3]]: {
--- 120-Coding Challenge 3/main.js
+++ 120-Coding Challenge 3/main.js
@@ -1,3 +1,25 @@
+// LAB2.1: (SECTION	9: DATA	STRUCTURES. MODERN	OPERATORS	AND	STRINGS	> CODING	
+//     CHANLLENGE	#3)
+//     Hãy tiếp tục với app đặt cược của chúng ta! Lúc này, chúng ta có 1 mảng kết hợp
+//     theo dạng [key,value] tương ứng với những đoạn log ghi lại những sự kiện xảy ra
+//     trong trận đấu.
+//     Trong đó:
+//     -> key = phút diễn ra
+//     -> value = tên hành động diễn ra (ghi bàn, thay người, nhận thẻ đỏ,...)
+//     Bây giờ chúng ta có 1 mảng liên kế gồm các sự kiện xảy ra trong trận đấu cùng
+//     thời gian tương ứng
+//     1. Hãy tạo ra 1 mảng các sự kiện khác nhau xảy ra trong suốt trận đấu (lưu ý là
+//     không trùng nhau)
+//     2. Sau khi trò chơi kết thúc, phát hiện ra rằng thẻ vàng phút 64 là không đúng. Hãy
+//     xóa nó khỏi danh sách sự kiện trong trận đấu.
+//     3. In ra console theo định dang như sau:"1 sự kiện xảy ra, trung bình mỗi 9 phút".
+//     Lưu ý răng mỗi trận đấu có 90 phút.
+//     LAB	2
+//     LẬP TRÌNH JAVASCRIPT NÂNG CAO TRANG 2
+//     4. Lặp toàn bộ mảng sự kiện và in ra màn hình, đánh dấu đối với mỗi sự kiện trong
+//     trận đấu diễn ra trong hiệp 1 hay hiệp 2,
+//     Ví dụ: [FIRST HALF] 17: ⚽ GOAL
+
 const gameEvent = new Map(
     [
         [17,'GOAL'],
