d72963220af9c6ea4a1b8ba4d0cb5d52cf95831e
hieunm <nguyenmanhhieutl@gmail.com>
2023-01-29 22:50:08 +0700

    update code

---
+++ 125- String Methods Practice/main.js
@@ -0,0 +1,33 @@
+// LAB2.3: (SECTION	9: DATA	STRUCTURES. MODERN	OPERATORS	AND	STRINGS	> STRING	
+//     METHOD	PRACTICE)
+//     Thực hiện code ví dụ sau
+//     // String Methods Practice
+//     const flights =
+     
+//     '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao937661
+//     09;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis
+//     2323639855;12:30';
+//     // � Delayed Departure from FAO to TXL (11h25)
+//     // Arrival from BRU to FAO (11h45)
+//     // � Delayed Arrival from HEL to FAO (12h05)
+//     // Departure from FAO to LIS (12h30)
+//     const getCode = str => str.slice(0, 3).toUpperCase();
+//     for (const flight of flights.split('+')) {
+//      const [type, from, to, time] = flight.split(';');
+//      const output = `${type.startsWith('_Delayed') ? '�' : ''}${type.replaceAll(
+//      '_',
+//      ' '
+//      )} ${getCode(from)} ${getCode(to)} (${time.replace(':', 'h')})`.padStart(36);
+//      console.log(output);
+//     }
+
+const flights=
+'_Delayed_Departure;fao93766109;tx12333758840;11:25 +_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis232639855;12:30';
+console.log(flights.split('+'));
+const getCode = str => str.slice(0,3).toUpperCase();
+for(const flight of flights.split('+')){
+  const[type, form, to, time]= flight.split(';');
+
+  const output=`${type.startsWith('_Delayed') ? '🔴' :' ' }${type.replaceAll('_',' ')} ${form.slice(0,3).toUpperCase()} ${to} ${time.replace(':','h')})`.padStart(36);
+  console.log(output);
+}
